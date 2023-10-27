b93423010c30a2133540bacb612d331c8a048c8a
Shinya Tahara <shinya@fastmail.com>
2023-10-27 16:41:20 +0900

    first commit

---
+++ 01_settings/sample_bedrock.py
@@ -0,0 +1,28 @@
+import json
+
+import boto3
+
+bedrock_runtime = boto3.client("bedrock-runtime", region_name="us-east-1")
+
+prompt = """
+Human: 大谷翔平について教えて
+
+Assistant:
+"""
+
+body = json.dumps(
+ {
+ "prompt": prompt,
+ "max_tokens_to_sample": 500,
+ }
+)
+
+resp = bedrock_runtime.invoke_model(
+ modelId="anthropic.claude-v2",
+ body=body,
+ contentType="application/json",
+ accept="application/json",
+)
+
+answer = resp["body"].read().decode()
+print(json.loads(answer)["completion"])
